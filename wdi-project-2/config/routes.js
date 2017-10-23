ab01a8df435c40e68b1dbf88dfc64fa86c804e4c
siobhanpotter <siobhanpotterone@gmail.com>
2017-10-24 00:09:48 +0100

    added nav and bootstrap

--- wdi-project-2/config/routes.js
+++ wdi-project-2/config/routes.js
@@ -4,61 +4,16 @@ const router  = express.Router();
 const registrations  = require('../controllers/registrations');
 const sessions  = require('../controllers/sessions');
 const exhibitions = require('../controllers/exhibitions');
+const secureRoute = require('../lib/secureRoute');
 
 
 router.get('/', (req, res) => res.render('homepage'));
 
-// router.route('/')
-//   .get(statics.index);
-
-// function secureRoute(req, res, next) {
-//   if (!req.session.userId) {
-//     return req.session.regenerate(() => {
-//       req.flash('danger', 'You must be logged in.');
-//       res.redirect('/login');
-//     });
-//   }
-
-//   return next();
-// }
-
-
-// router.get('/exhibitions', (req, res) => {
-//   Exhibition
-//     .find()
-//     .exec()
-//     .then((exhibitions) => {
-//       res.render('exhibitions', { exhibitions });
-//     })
-//     .catch((err) => {
-//       res.status(500).end(err);
-//     });
-// });
-
 router.route('/exhibitions')
   .get(exhibitions.create);
 
-
 router.route('/exhibitions/:id')
   .get(exhibitions.showOne);
-
-
-// .put(secureRoute, exhibitionsController.update)
-// .delete(secureRoute, exhibitionsController.delete);
-
-// router.get('/exhibitions/:name', (req, res) => {
-//   Exhibition
-//     .findOne({ name: req.params.name })
-//     .exec()
-//     .then((exhibition) => {
-//       if(!exhibition) return res.status(404).end();
-//       res.render('exhibition', { exhibition });
-//     })
-//     .catch(() => {
-//       res.status(500).end();
-//     });
-// });
-
 
 router.route('/register')
   .get(registrations.new)
@@ -73,20 +28,11 @@ router.route('/logout')
 
 //****************************************************************************
 router.route('/exhibitions/:id/comments')
-  .post(exhibitions.createComment);
+  .post(secureRoute, exhibitions.createComment);
 
 router.route('/exhibitions/:id/comments/:commentId')
-  .delete(exhibitions.deleteComment);
+  .delete(secureRoute, exhibitions.deleteComment);
 //*****************************************************************************
 
-// RESTful routes
-// All URLS should contain the PLURAL... don't chose octopus or people or something silly.
-// INDEX
-// NEW
-// SHOW
-// CREATE
-// EDIT
-// UPDATE
-// DELETE
 
 module.exports = router;
